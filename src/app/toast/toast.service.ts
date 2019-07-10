453fdf28502906622a3517f3214dee4c1c2c218d
James Allen <james.allen@meta.ceo>
2019-07-09 21:14:21 -0500

    Showing and closing toast component.

--- src/app/toast/toast.service.ts
+++ src/app/toast/toast.service.ts
@@ -1,5 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { ApplicationRef, EmbeddedViewRef, Inject, Injectable, Injector, NgModuleRef } from '@angular/core';
+import { ApplicationRef, ComponentRef, EmbeddedViewRef, Inject, Injectable, Injector, NgModuleRef } from '@angular/core';
 
 import { ToastComponent } from './toast.component';
 import { ToastModule } from './toast.module';
@@ -7,28 +7,42 @@ import { ToastModule } from './toast.module';
 @Injectable()
 export class ToastService {
 
+  private closingSetTimeout: any;
+  private componentRef: ComponentRef<ToastComponent>;
+
   constructor(private readonly applicationRef: ApplicationRef,
               private readonly injector: Injector,
               private readonly toastModuleNgModuleRef: NgModuleRef<ToastModule>,
               @Inject(DOCUMENT) private readonly document: Document) {
   }
 
-  public open(message: string): void {
-    // Create the component factory that provides us the component reference:
-    const componentFactory = this.toastModuleNgModuleRef.componentFactoryResolver.resolveComponentFactory(ToastComponent);
-    const componentRef = componentFactory.create(this.injector);
+  public open(message: string, timeout = 5000): void {
 
-    // Use the component reference and add it to the Angular application + DOM:
-    this.applicationRef.attachView(componentRef.hostView);
-    const domElem = (componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
-    this.document.body.appendChild(domElem);
+    // Clear any previous closing toast:
+    if (this.closingSetTimeout) {
+      clearTimeout(this.closingSetTimeout);
+    }
 
-    // Do message:
-    componentRef.instance.message = message;
+    // If a toast component hasn't been made, make one:
+    if (!this.componentRef) {
+      // Create the component factory that provides us the component reference:
+      const componentFactory = this.toastModuleNgModuleRef.componentFactoryResolver.resolveComponentFactory(ToastComponent);
+      this.componentRef = componentFactory.create(this.injector);
 
-    // Remove message:
-    this.applicationRef.detachView(componentRef.hostView);
-    componentRef.destroy();
+      // Use the component reference and add it to the Angular application + DOM:
+      this.applicationRef.attachView(this.componentRef.hostView);
+      const domElem = (this.componentRef.hostView as EmbeddedViewRef<any>).rootNodes[0] as HTMLElement;
+      this.document.body.appendChild(domElem);
+    }
+
+    // Pass toast message:
+    this.componentRef.instance.message = message;
+
+    // Remove toast component:
+    this.closingSetTimeout = setTimeout(() => {
+      this.applicationRef.detachView(this.componentRef.hostView);
+      this.componentRef.destroy();
+    }, timeout);
   }
 
 }
