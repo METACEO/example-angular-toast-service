96a6279888a23e346fb21d61427861a9d514c9aa
James Allen <james.allen@meta.ceo>
2019-07-09 20:46:33 -0500

    yarn ng g component toast

--- src/app/toast/toast.module.ts
+++ src/app/toast/toast.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToastComponent } from './toast.component';
 
 @NgModule({
-  declarations: [],
+  entryComponents: [ToastComponent],
   imports: [
     CommonModule
   ]
