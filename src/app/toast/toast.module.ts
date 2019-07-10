0601bfa96485bdda00c824506a1cbc49027dfc99
James Allen <james.allen@meta.ceo>
2019-07-09 20:48:02 -0500

    yarn ng g service toast

--- src/app/toast/toast.module.ts
+++ src/app/toast/toast.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ToastComponent } from './toast.component';
+import { ToastService } from './toast.service';
 
 @NgModule({
   entryComponents: [ToastComponent],
+  providers: [
+    ToastService
+  ],
   imports: [
     CommonModule
   ]
